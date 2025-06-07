3f5233e98ff6a4f5610da60c3f31f6e0d139bab0
VishalMer <vishalmer99747@gmail.com>
2025-06-07 17:57:33 +0530

    initial commit

---
+++ env.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+console.log(process.env.NAME);
+console.log(process.env.PROFESSION);
+console.log("I'm starting new series on",process.env.COURSE);
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("This is a Nodejs practice.");
+console.log("This is first tutorial on Nodejs.");
+console.log("Hello i'm Vishal Mer");
+
+//process.exit(1);
+//process.exitCode = 1; // 0 will do a programatic exit, 1 will do crash the app
