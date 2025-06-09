e0c68d4332a73ab9a900e0d7a4edb9bc615ab17f
Mer Vishal <vishalmer99747@gmail.com>
2025-06-09 11:25:15 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,15 @@
+const readLine = require("readline");
+
+const rl = readLine.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
+// rl.question(`What is your name ?`, (name) => {
+//     console.log(`Hi ${name}`);
+//     rl.close();
+// });  
+
+const Prompt = require("prompt-sync")();
+const name = Prompt("What is your name ?");
+console.log(`Hi ${name}`);
