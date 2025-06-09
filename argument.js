1cac23f123ee432274754dfdd5aa4cccf19419da
VishalMer <vishalmer99747@gmail.com>
2025-06-09 11:26:29 +0530

    Basics of Node JS 2

---
+++ argument.js
@@ -0,0 +1,14 @@
+// console.log(process.argv);
+
+//console.log(process.argv.slice(2)[0]);
+
+
+// process.argv.forEach((val,index) => {
+//     console.log(`${index}:${val}`);
+// });
+
+const minimist = require("minimist");
+const newArg = minimist(process.argv.slice(2));
+console.log(newArg.name);
+//run by = node argument.js --name=Vishal
+//vishal will be printed
