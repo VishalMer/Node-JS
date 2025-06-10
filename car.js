7682bb4afdb43d834e12818050b33f6ad82443c5
Mer Vishal <vishalmer99747@gmail.com>
2025-06-10 11:33:55 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,13 @@
+const Ford = {
+    branch : "Ford",
+    model : "Endevour", 
+}
+
+const RR = {
+    branch : "Rollce Royce",
+    model : "Ghost", 
+}
+
+//module.exports = Ford;
+
+exports.car = {Ford,RR};
