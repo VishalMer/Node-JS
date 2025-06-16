662b57611b21a83105da21421ce168855cdfd209
VishalMer <vishalmer99747@gmail.com>
2025-06-16 22:02:19 +0530

    ...

--- path.js
+++ path.js
@@ -18,10 +18,30 @@ const filePath = "E:\Projects\Node JS\Files\Sample.txt";
   //Filesystem
 //console.log(fs); //Displays filesystem methods
 
-//Reading from a file
+//Reading from a file - Async
 const fs = require("fs");
- fs.readFile(filePath, (err, data) => {
-//fs.readFile(filePath, "utf-8", (err, data) => {
-    //if(err) throw new Error("Somehting went wrong!");
-    console.log(data);
-});
+//  fs.readFile(filePath, (err, data) => {
+// //fs.readFile(filePath, "utf-8", (err, data) => {
+//     if(err) throw new Error("Somehting went wrong!");
+//     console.log(data);
+// });
+
+// try{
+//   const data = fs.readFileSync(path.join(__dirname, "Files", "Sample.txt"),"utf-8");
+//   console.log(data);
+// }catch(err){
+//   console.log(err);
+// }
+
+
+const fsPromise = require("fs").promises;
+const fileReading = async() => {
+  try {
+    const data = await fsPromise.readFile(filePath, {encoding:"utf-8"});
+    console.log("FS Promises : ", data);
+  }catch(err){
+    console.log(err);
+  }
+}
+
+fileReading();
