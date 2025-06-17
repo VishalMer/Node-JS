e88ae159d54fb7e825c3678dff1feb573cb0469f
VishalMer <vishalmer99747@gmail.com>
2025-06-17 16:16:40 +0530

    Updated File by Solving Errors

--- path.js
+++ path.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const filePath = "E:\Projects\Node JS\Files\Sample.txt";
+const filePath = "E:\\Projects\\Node JS\\Files\\Sample.txt";
 
 // //Dir Name
 // console.log(path.dirname(filePath));
@@ -15,13 +15,14 @@ const filePath = "E:\Projects\Node JS\Files\Sample.txt";
 // const SampleFile = "Sample.txt";
 // console.log(path.join(path.dirname(filePath), SampleFile));
 
+
   //Filesystem
 //console.log(fs); //Displays filesystem methods
 
 //Reading from a file - Async
-const fs = require("fs");
-//  fs.readFile(filePath, (err, data) => {
-// //fs.readFile(filePath, "utf-8", (err, data) => {
+// const fs = require("fs");
+// // fs.readFile(filePath, (err, data) => {
+// fs.readFile(filePath, "utf-8", (err, data) => {
 //     if(err) throw new Error("Somehting went wrong!");
 //     console.log(data);
 // });
@@ -34,14 +35,14 @@ const fs = require("fs");
 // }
 
 
-const fsPromise = require("fs").promises;
-const fileReading = async() => {
-  try {
-    const data = await fsPromise.readFile(filePath, {encoding:"utf-8"});
-    console.log("FS Promises : ", data);
-  }catch(err){
-    console.log(err);
-  }
-}
+// const fsPromise = require("fs").promises;
+// const fileReading = async() => {
+//   try {
+//     const data = await fsPromise.readFile(filePath, {encoding:"utf-8"});
+//     console.log("FS Promises : ", data);
+//   }catch(err){
+//     console.log(err);
+//   }
+// }
 
-fileReading();
+// fileReading();
