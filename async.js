d53dfd0d8c6580f64e6aaa9658fbcfd648a77c24
VishalMer <vishalmer99747@gmail.com>
2025-06-18 12:03:07 +0530

    Sync VS Async

---
+++ async.js
@@ -0,0 +1,29 @@
+//syncronous
+// console.log("Start");
+
+// function sleep(milliseconds){
+//     let startTime = new Date().getTime();
+//     console.log("operation is running.");
+//     while (new Date().getTime() < startTime + milliseconds){
+//         console.log("In progress");
+//     }
+//     console.log("Operation is done.");
+// }
+
+// sleep(1000);
+// console.log("do Something else...");
+
+
+//asncrounous manner
+console.log("Start");
+
+function sleep(milliseconds){
+    console.log("operation is running.");
+    setTimeout(() => {
+            console.log("Operation is done.");
+
+    },milliseconds);
+}
+
+sleep(1000);
+console.log("do Something else...");
