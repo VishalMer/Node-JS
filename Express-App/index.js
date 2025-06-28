546ab992a6a2315e4e2ed77fb05787b4a4af9ac0
VishalMer <vishalmer99747@gmail.com>
2025-06-28 16:14:05 +0530

    Get started with express - sample express app created

--- Express-App/index.js
+++ Express-App/index.js
@@ -3,8 +3,28 @@ const app = express();
 
 const port = 3001;
 
-app.get("/", (req, res) ={
-    res.send("Hello Mer!");
+app.get("/", (req, res) => {
+    res.json({message : "This is home page"});
+});
+
+app.get("/users", (req, res) => {
+    res.json({"message" : "Get all the users"});
+});
+
+app.get("/users/:id", (req, res) => {
+    res.json({message : `Get user with id ${req.params.id}`});
+});
+
+app.post("/users", (req, res) => {
+    res.json({message : "Create new user"});
+});
+
+app.put("/users/:id", (req, res) => {
+    res.json({message : `Update user with id ${req.params.id}`});
+});
+
+app.delete("/users/:id", (req, res) => {
+    res.json({message : `Delete user with id ${req.params.id}`});
 });
 
 app.listen(port, () => {
